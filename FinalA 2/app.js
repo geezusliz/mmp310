c8cdea073c7998894f48e106435ea07b3ba88ce3
Elizabeth Gomez <elizabeth.gomez2@f1006-30-34172.bmcc.cuny.edu>
2018-12-10 18:45:53 -0500

    updated final

--- FinalA 2/app.js
+++ FinalA 2/app.js
@@ -1,5 +1,6 @@
 const submitButton = document.getElementById('submit');
 const vidInput = document.getElementById('vid');
+const playButton = document.getElementById('play');
 
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
@@ -8,21 +9,27 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 submitButton.onclick = function () {
     const vid = vidInput.value.split("=")[1];
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < 20; i++) {
         let div = document.createElement('div');
         div.classList.add("video");
         document.getElementById('videos').appendChild(div);
         const player = new YT.Player(div, {
             videoId: vid,
             events: {
-                'onReady': onPlayerReady
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
             }
         });
 
         function onPlayerReady() {
             player.playVideo();
-
         }
+         function onPlayerStateChange(event) {
+    if (event.data == 0) {
+            player.playVideo();
     }
+         }
+    }
+    
 
 }
